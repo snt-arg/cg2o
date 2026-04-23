3fab39b9220b8e85c614c27a8a0a9c01abfc83ca
Anas-Abdelkarim <54266723+Anas-Abdelkarim@users.noreply.github.com>
2026-04-23 14:06:12 +0200

    Update docker startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 1. Setup the shell environment
-if [ -f /ws/docker/bashSetup.sh ]; then
-    source /ws/docker/bashSetup.sh
+if [ -f /ws/scripts/bashSetup.sh ]; then
+    source /ws/scripts/bashSetup.sh
 fi
 
 # 2. Display the instructions
-if [ -f /ws/docker/welcomeMessage.sh ]; then
-    bash /ws/docker/welcomeMessage.sh
+if [ -f /ws/scripts/welcomeMessage.sh ]; then
+    bash /ws/scripts/welcomeMessage.sh
 fi
 
 # 3. Stay in the shell
