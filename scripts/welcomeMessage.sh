d0820ff83c1c7244105f61eba0403ae7cf5cf5c8
Anas-Abdelkarim <54266723+Anas-Abdelkarim@users.noreply.github.com>
2026-04-23 15:03:03 +0200

    update welcomeMessage

--- scripts/welcomeMessage.sh
+++ scripts/welcomeMessage.sh
@@ -7,7 +7,11 @@ cat <<'EOF'
 CG2O Development Container
 ============================================================
 
+
+
+
 ## To build the CG2O examples, run:
+====================================
 
   mkdir -p /ws/build && cd /ws/build && \
   rm -rf CMakeFiles  &&  rm -f CMakeCache.txt &&\
@@ -31,18 +35,26 @@ CG2O Development Container
   /ws/build/cg2o/examples/example_1 150 0 0 
   /ws/build/cg2o/examples/example_2
   /ws/build/cg2o/examples/example_3
+  
+  
 
 ## To build the MPC project:
-
+====================================
   cd /ws/app/cpp_code  && \
   mkdir -p build && cd build && \
   rm -rf CMakeFiles  &&  rm -f CMakeCache.txt && \
+  cd ../build 
   cmake .. \
     -DMPC_FEASIBLE_INITIALIZATION==OFF \
-    -DMPC_CG2O_OPTIMIZER=BIPM\
-    -DMPC_USE_NUMERICAL_JACOBIANS=OFF \  
-  && \
-  make -j$(nproc)
+    -DMPC_CG2O_OPTIMIZER=ISPD\
+    -DMPC_USE_NUMERICAL_JACOBIANS=OFF \
+     && \
+     make -j$(nproc)
+  
+   
+ with   -DMPC_FEASIBLE_INITIALIZATION==ON \
+     use       -DMPC_CG2O_OPTIMIZER=BIPM\
+
 
 ## To change the Controller and the Solver paramter modify the yaml on the path:
   /ws/app/cpp_code/config/config.yaml
@@ -50,8 +62,10 @@ CG2O Development Container
   /ws/app/cpp_code/results/data/mpc_g2o_exc
  
 
-## To build the ROS package:
 
+
+## To build the ROS package:
+===================================================
   source /opt/ros/jazzy/setup.bash  && \
   cd /ws/app/ros_ws  && \
   rm -rf build install log  && \
@@ -63,7 +77,7 @@ CG2O Development Container
 
 	ros2 run acc_control_mpc_g2o acc_control_mpc_g2o_ros --ros-args -p numberOfIterations:=40 -p horizon_length:=3 & \
 	sleep 2; \
-	ros2 action send_goal /acc_control_mpc_g2o_action acc_interfaces/action/AccControlMPC "{horizon_length: 3, v_p: 15.0, v_h: 12.0, a_p: 0.5, a_p_weighted: 0.5, d_h: 4.5, force_prev: 10.0}";\
+	ros2 action send_goal /acc_control_mpc_g2o_action acc_interfaces/action/AccControlMPC "{horizon_length: 3, v_p: 15.0, v_h: 12.0, a_p: 0.5, a_p_weighted: 0.5, d_h: 14.5, force_prev: 10.0}";\
 	pkill -9 -f acc_control_mpc_g2o_ros
 
 ## Useful paths:
